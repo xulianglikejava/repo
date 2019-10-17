051ec555f21577398bf4de3e444f58d0d0f2916e
xuliang <xx1271328330@163.com>
2019-10-17 18:47:33 +0800

    add

---
+++ ssm_consums/src/main/main/java/cn/itcast/controller/hello/demo.java
@@ -0,0 +1,11 @@
+package cn.itcast.controller.hello;
+
+/**
+ * @author xuLiang
+ * @date 2019/10/17 18:47
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
